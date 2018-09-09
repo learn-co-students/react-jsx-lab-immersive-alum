d8aec760340289bbc4de7bb8904087f712754fc0
= <=>
2018-09-09 13:29:47 -0400

    Tony Solution

---
+++ src/components/Webpage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FillerText from './FillerText'
+
+export default () => (
+  <div>
+    <h3>The world's coolest webpage</h3>
+    <FillerText />
+    <FillerText />
+  </div>
+)
